2cb1fbe8ddafe66d0a58405c21e5f7419c9125f5
Mike Lankamp <mike.lankamp@gmail.com>
2024-11-05 00:32:20 +0100

    feat: add support for Steam's Empire at War Gold Pack
    The AloViewer will now find EaW and FoC (and mods) if Star Wars: Empire at War Gold Pack is installed in Steam.

--- src/Games.cpp
+++ src/Games.cpp
@@ -1,71 +1,261 @@
 #include "Games.h"
 #include <shlwapi.h>
-using namespace std;
+#include <array>
+#include <cctype>
+#include <fstream>
+#include <optional>
+#include <filesystem>
+#include <sstream>
+#include <unordered_map>
+#include <variant>
 
-// Returns the base directory for a game, based on
-// 1. Registry
-// 2. Default path, expanded
-static wstring GetExePath(const wchar_t* regkey, const wchar_t* valname, const wchar_t* defpath = NULL)
-{
-	// The games are 32-bit games, so make these calls in 32-bit mode
-    wstring result;
-	
-    HKEY hKey;
-	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, regkey, 0, KEY_QUERY_VALUE | KEY_WOW64_32KEY, &hKey ) == ERROR_SUCCESS)
-	{
-		DWORD type, size = MAX_PATH;
-        TCHAR path[MAX_PATH];
-		if (RegQueryValueEx(hKey, valname, NULL, &type, (LPBYTE)path, &size) == ERROR_SUCCESS)
-		{
-            if (PathRemoveFileSpec(path) && PathIsDirectory(path))
-            {
-				result = path;
+namespace {
+    class VdfParser {
+    public:
+        struct KeyValue;
+        using KeyValues = std::vector<KeyValue>;
+        struct KeyValue {
+            std::string key;
+            std::variant<std::string, KeyValues> value;
+        };
+
+        explicit VdfParser(const std::filesystem::path& path) {
+            file.exceptions(std::ios::badbit | std::ios::failbit);
+            file.open(path);
+        }
+
+        std::optional<KeyValue> Parse() {
+            if (auto key = next()) {
+                if (*key == "}") {
+                    // We're done
+                    return {};
+                }
+
+                if (auto value = next()) {
+                    KeyValue result;
+                    if (*value == "{") {
+                        KeyValues values;
+                        while (auto kv = Parse()) {
+                            values.push_back(std::move(*kv));
+                        }
+                        result.value = std::move(values);
+                    }
+                    else {
+                        result.value = std::move(*value);
+                    }
+                    result.key = std::move(*key);
+                    return result;
+                }
             }
-		}
-		RegCloseKey(hKey);
-	}
-    
-    if (result.empty() && defpath != NULL)
+            return {};
+        }
+
+    private:
+        std::optional<std::pair<std::string, std::string>> next_pair() {
+            if (auto key = next()) {
+                if (auto value = next()) {
+                    return std::make_pair<std::string, std::string>(std::move(*key), std::move(*value));
+                }
+            }
+            return {};
+        }
+
+        std::optional<std::string> next() {
+            int ch;
+            while (std::isspace(ch = file.get())) {}
+            if (ch == -1) {
+                return {};
+            }
+
+            std::stringstream token;
+            if (ch == '"') {
+                while ((ch = file.get()) != '"') {
+                    if (ch == '\\') {
+                        if ((ch = file.get()) == -1) {
+                            return {};
+                        }
+                    }
+                    token << (char)ch;
+                }
+            }
+            else {
+                token << (char)ch;
+                while (!std::isspace(ch = file.get())) {
+                    if (ch == -1) {
+                        return {};
+                    }
+                    token << (char)ch;
+                }
+            }
+            return token.str();
+        }
+
+        std::ifstream file;
+    };
+
+    std::optional<std::string> FindValue(const VdfParser::KeyValues& key_values, const std::string& key)
     {
-        // Use the default path
+        for (const auto& kv: key_values) {
+            if (kv.key == key) {
+                if (const auto* val = std::get_if<std::string>(&kv.value)) {
+                    return *val;
+                }
+            }
+        }
+        return {};
+    }
+
+    // Returns the base directory for a game, based on
+    // 1. Registry
+    // 2. Default path, expanded
+    std::optional<std::filesystem::path> GetRegistryPath(const wchar_t* regkey, const wchar_t* valname)
+    {
+        // The games are 32-bit games, so make these calls in 32-bit mode
+        HKEY hKey;
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, regkey, 0, KEY_QUERY_VALUE | KEY_WOW64_32KEY, &hKey) == ERROR_SUCCESS)
+        {
+            DWORD type, size = MAX_PATH;
+            TCHAR path[MAX_PATH];
+            if (RegQueryValueEx(hKey, valname, NULL, &type, (LPBYTE)path, &size) == ERROR_SUCCESS)
+            {
+                RegCloseKey(hKey);
+                return path;
+            }
+            RegCloseKey(hKey);
+        }
+        return {};
+    }
+
+    std::optional<std::filesystem::path> GetDirectoryPath(std::optional<std::filesystem::path> path)
+    {
+        if (path) {
+            path->remove_filename();
+            if (!std::filesystem::is_directory(*path)) {
+                return {};
+            }
+        }
+        return path;
+    }
+
+    std::optional<std::filesystem::path> GetEnvironmentPath(const wchar_t* envpath) {
         TCHAR path[MAX_PATH];
-        if (ExpandEnvironmentStrings(defpath, path, MAX_PATH) != 0)
+        if (ExpandEnvironmentStrings(envpath, path, MAX_PATH) != 0)
         {
             if (PathIsDirectory(path))
             {
-                result = path;
+                return path;
             }
         }
+        return {};
     }
 
-    return result;
-}
+    std::vector<std::filesystem::path> GetSteamLibraryPaths() {
+        std::vector<std::filesystem::path> library_paths;
+        if (const auto steam_path = GetRegistryPath(L"Software\\Valve\\Steam", L"InstallPath")) {
+            if (const auto root = VdfParser(*steam_path / "steamapps" / "libraryfolders.vdf").Parse()) {
+                if (root->key == "libraryfolders") {
+                    if (const auto* folders = std::get_if<VdfParser::KeyValues>(&root->value)) {
+                        for (const auto& folder : *folders) {
+                            if (const auto* folder_items = std::get_if<VdfParser::KeyValues>(&folder.value)) {
+                                if (auto path = FindValue(*folder_items, "path")) {
+                                    if (std::filesystem::is_directory(*path)) {
+                                        library_paths.push_back(std::move(*path));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return library_paths;
+    }
 
-// Returns the base directory for a game
-static wstring GetBaseDirForGame(GameID game)
-{
-    switch (game)
+    std::optional<std::filesystem::path> GetSteamAppPath(unsigned int app_id, const wchar_t* subdir = nullptr) {
+        try {
+            for (const auto& library_path : GetSteamLibraryPaths()) {
+                try {
+                    if (const auto root = VdfParser(library_path / "steamapps" / ("appmanifest_" + std::to_string(app_id) + ".acf")).Parse()) {
+                        if (root->key == "AppState") {
+                            if (const auto* app_items = std::get_if<VdfParser::KeyValues>(&root->value)) {
+                                if (const auto dirname = FindValue(*app_items, "installdir")) {
+                                    auto app_path = library_path / "steamapps" / "common" / *dirname;
+                                    if (subdir) {
+                                        app_path /= subdir;
+                                    }
+                                    if (std::filesystem::is_directory(app_path)) {
+                                        return app_path;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (const std::exception&) {
+                }
+            }
+        }
+        catch (const std::exception&) {
+        }
+        return {};
+    }
+
+    // Calls every provider in order and returns the first path found. Returns empty string if no paths are found.
+    template <typename... PathProviders>
+    std::wstring FindFirstPath(PathProviders ...path_providers)
     {
+        try {
+            ([&] {
+                if (auto path = path_providers()) {
+                    throw std::move(*path);
+                }
+                }(), ...);
+        }
+        catch (const std::filesystem::path& path) {
+            return path.wstring();
+        }
+        return {};
+    }
+
+    unsigned int SteamAppId_EmpireAtWarGoldPack = 32470;
+
+    // Returns the base directory for a game
+    std::wstring GetBaseDirForGame(GameID game)
+    {
+        switch (game)
+        {
         case GID_EAW_FOC:
-            return GetExePath(L"Software\\LucasArts\\Star Wars Empire at War Forces of Corruption\\1.0", L"ExePath", L"%PROGRAMFILES%\\LucasArts\\Star Wars Empire at War Forces of Corruption");
+            return FindFirstPath(
+                [] { return GetDirectoryPath(GetRegistryPath(L"Software\\LucasArts\\Star Wars Empire at War Forces of Corruption\\1.0", L"ExePath")); },
+                [] { return GetSteamAppPath(SteamAppId_EmpireAtWarGoldPack, L"corruption"); },
+                [] { return GetEnvironmentPath(L"%PROGRAMFILES%\\LucasArts\\Star Wars Empire at War Forces of Corruption"); }
+            );
 
         case GID_EAW:
-            return GetExePath(L"Software\\LucasArts\\Star Wars Empire at War\\1.0", L"ExePath", L"%PROGRAMFILES%\\LucasArts\\Star Wars Empire at War\\GameData");
+            return FindFirstPath(
+                [] { return GetDirectoryPath(GetRegistryPath(L"Software\\LucasArts\\Star Wars Empire at War\\1.0", L"ExePath")); },
+                [] { return GetSteamAppPath(SteamAppId_EmpireAtWarGoldPack, L"GameData"); },
+                [] { return GetEnvironmentPath(L"%PROGRAMFILES%\\LucasArts\\Star Wars Empire at War\\GameData"); }
+            );
 
         case GID_UAW_EA:
-            return GetExePath(L"Software\\Petroglyph\\UAWEA", L"GameExe", L"%PROGRAMFILES%\\Sega\\Universe At War Earth Assault");
+            return FindFirstPath(
+                [] { return GetDirectoryPath(GetRegistryPath(L"Software\\Petroglyph\\UAWEA", L"GameExe")); },
+                [] { return GetEnvironmentPath(L"%PROGRAMFILES%\\Sega\\Universe At War Earth Assault"); }
+            );
+        }
+        return L"";
     }
-    return L"";
 }
 
 // Returns the GameMod identifier for a file
 GameMod GameMod::GetForFile(const std::wstring& path)
 {
-    vector<GameMod> gamemods;
+    std::vector<GameMod> gamemods;
     GetAllGameMods(gamemods);
     for (size_t i = 0; i < gamemods.size(); i++)
     {
-        wstring dir = gamemods[i].GetBaseDir();
+        std::wstring dir = gamemods[i].GetBaseDir();
         if (path.find(dir) == 0)
         {
             return gamemods[i];
@@ -75,13 +265,13 @@ GameMod GameMod::GetForFile(const std::wstring& path)
 }
 
 // Returns the base directory for a game or mod
-wstring GameMod::GetBaseDir() const
+std::wstring GameMod::GetBaseDir() const
 {
-    wstring path = GetBaseDirForGame(m_game);
+    std::wstring path = GetBaseDirForGame(m_game);
     if (!m_mod.empty())
     {
         // It's a mod, append modpath
-        wstring modpath = path + L"\\Mods\\" + m_mod;
+        std::wstring modpath = path + L"\\Mods\\" + m_mod;
         if (PathIsDirectory(modpath.c_str())) {
             return modpath;
         }
@@ -92,7 +282,7 @@ wstring GameMod::GetBaseDir() const
 // Appends the asset search directories for this game mod to the list
 void GameMod::AppendAssetDirs(std::vector<std::wstring>& basedirs) const
 {
-    wstring gamepath, modpath;
+    std::wstring gamepath, modpath;
     switch (m_game)
     {
     case GID_EAW_FOC:
@@ -144,9 +334,9 @@ bool GameMod::IsValid() const
 // Appends the game and the mods for the game to the list
 static void GetMods(GameID game, std::vector<GameMod>& gamemods)
 {
-    wstring gamepath;
+    std::wstring gamepath;
     if (!(gamepath = GetBaseDirForGame(game)).empty()) {
-        wstring modpath = gamepath + L"\\Mods\\*.*";
+        std::wstring modpath = gamepath + L"\\Mods\\*.*";
        
         WIN32_FIND_DATA wfd;
         HANDLE hFind = FindFirstFile(modpath.c_str(), &wfd);
